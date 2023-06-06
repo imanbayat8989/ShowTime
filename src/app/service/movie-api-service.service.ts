a6a554977900becc9365d20757e4b9ac3ed7c1fd
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-06-06 14:24:02 +0430

    working

--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -33,4 +33,19 @@ export class MovieApiServiceService {
     return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apikey}&query=${data.movieName}`);
   }
 
+  getMovieDetails(data:any):Observable<any>
+  {
+    return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apikey}`);
+  }
+
+  getMovieVideo(data:any):Observable<any>
+  {
+    return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apikey}`);
+  }
+
+  getMovieCast(data:any):Observable<any>
+  {
+    return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`);
+  }
+
 }
